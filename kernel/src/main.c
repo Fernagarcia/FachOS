a8a763fe5d4ce00d852666f2b926b85880428b98
Fernagarcia <fernagarcia@frba.utn.edu.ar>
2024-04-08 22:51:55 +0000

    Add: Creo log y creo config en Kernel

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,8 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <utils/server.h>
-#include <utils/client.h>
+#include </home/utnso/so-commons-library/tpSO/tp-2024-1c-Grupo-Facha/utils/src/utils/server.h>
+#include </home/utnso/so-commons-library/tpSO/tp-2024-1c-Grupo-Facha/utils/src/utils/client.h>
+
 
 int main(int argc, char* argv[]) {
+    
+    t_log* logger = iniciar_logger();
+
+    t_config* config = iniciar_config;
+
+    log_destroy(logger);
+
+    config_destroy(config);
+
     return 0;
+}
+
+t_log* iniciar_logger(void)
+{
+	t_log* nuevo_logger;
+
+	nuevo_logger = log_create("Kernel-Log.log", "Cliente-Kernel", 1, LOG_LEVEL_INFO);
+	
+	return nuevo_logger;
+}
+
+t_config* iniciar_config(void)
+{
+    t_config* nuevo_config;
+
+    nuevo_config = config_create("/home/utnso/so-commons-library/tpSO/tp-2024-1c-Grupo-Facha/kernel/src");
+
+    return nuevo_config;
 }
