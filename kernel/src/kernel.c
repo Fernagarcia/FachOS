9301e535a2258a65af5a1c6ee8f651811c8d1ae0
Fernagarcia <fernagarcia@frba.utn.edu.ar>
2024-04-12 23:38:42 +0000

    ADD: Conexion entre entrada/salida(cliente) y kernel(server)

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -2,22 +2,23 @@
 
 int main(int argc, char* argv[]) {
     char* path_config = "../kernel/kernel.config";
+    char* puerto_escucha;
 
     // CREAMOS LOG Y CONFIG
 
-    t_log* logger = iniciar_logger("kernel.log", "kernel-log", LOG_LEVEL_INFO);
-    
-    log_info(logger, "hola xd");
+    t_log* logger_kernel = iniciar_logger("kernel.log", "kernel-log", LOG_LEVEL_INFO);
+    log_info(logger_kernel, "Logger Creado.");
 
     t_config* config = iniciar_config(path_config);
-    
-    abrir_servidor(logger);
+    puerto_escucha = config_get_string_value(config, "PUERTO_ESCUCHA");
 
-    log_destroy(logger);
+    abrir_servidor(logger_kernel, puerto_escucha);
+
+    log_destroy(logger_kernel);
     
     config_destroy(config);
 
-    //terminar_programa(conexion, logger, config);
+    //terminar_programa(conexion, logger_kernel, config);
 
     return 0;
 }
--- kernel/src/kernel.h
+++ kernel/src/kernel.h
@@ -1,10 +1,6 @@
 #ifndef KERNEL_H_
 #define KERNEL_H_
 
-#include <utils/server.h>
-
-#include <utils/client.h>
-
 #include <utils/utils.h>
 
 #endif
