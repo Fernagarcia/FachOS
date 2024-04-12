dd0d26d0b505158be16cbf71debdfd114c30985d
LucianoRocchetta <lrocchetta@frba.utn.edu.ar>
2024-04-12 18:47:51 +0000

    Fix: Arreglo de importación

--- kernel/src/kernel.h
+++ kernel/src/kernel.h
@@ -1,14 +1,9 @@
 #ifndef KERNEL_H_
 #define KERNEL_H_
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <utils/utils.h>
 #include <utils/server.h>
 #include <utils/client.h>
-#include <utils/utils.h>
-
-t_log* iniciar_logger(char* log_path, char* log_name, t_log_level log_level);
-t_config* iniciar_config(char* config_path);
 
 #endif
 
