7f74315478055db460f4db64bbf62a606d2fe848
SantyPerezzz <santi.agus.perez.2002@gmail.com>
2024-04-12 16:58:49 +0000

    add: kernel.c code cpu.c code

---
+++ entradasalida/src/entradasalida.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <utils/server.h>
+#include <utils/client.h>
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,8 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <utils/hello.h>
+#include <utils/server.h>
+#include <utils/client.h>
+<<<<<<< HEAD
+
+=======
+>>>>>>> 4742dbe2ded81bcf5371eb5cf0f1c4c45d047707
 
 int main(int argc, char* argv[]) {
-    decir_hola("Kernel");
+    
+    t_log* logger = iniciar_logger();
+
+    t_config* config = iniciar_config();
+
+    log_destroy(logger);
+
+    config_destroy(config);
+
     return 0;
+}
+
+t_log* iniciar_logger(void)
+{
+	t_log* nuevo_logger;
+
+	nuevo_logger = log_create("Kernel-Log.log", "Cliente-Kernel", 1, LOG_LEVEL_INFO);
+	
+	return nuevo_logger;
+}
+
+t_config* iniciar_config(void)
+{
+    t_config* nuevo_config;
+
+    nuevo_config = config_create("kernel/src");
+
+    return nuevo_config;
 }
