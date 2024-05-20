855dcef0431b0b9d73f186e47f44d5202173f089
SantyPerezzz <santi.agus.perez.2002@gmail.com>
2024-05-20 23:49:46 +0000

    Commiteo ideas para las interfaces de IO

--- entradasalida/src/io_generica.h
+++ entradasalida/src/io_generica.h
@@ -3,6 +3,11 @@
 
 #include <utils/utils.h>
 
+typedef struct {
+    const char *name;
+    t_config *configuration;
+} IO_GENERICA;
+
 void iniciar_interfaz(char* nombre, t_config* config);
 
 #endif
