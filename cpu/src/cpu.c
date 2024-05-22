1e5cd644b0905710e31ca468447990a3c8f13aa1
SantyPerezzz <santi.agus.perez.2002@gmail.com>
2024-05-22 22:28:37 +0000

    modify: hago los puertos dispatch e interrupt variables globales

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -2,6 +2,8 @@
 #include <utils/parse.h>
 
 int conexion_memoria;
+int server_interrupt;
+int server_dispatch;
 
 char* instruccion_a_ejecutar;
 
@@ -26,6 +28,62 @@ INSTRUCTION instructions[] = {
     { "IO_STDIN_READ", io_stdin_read, "Ejecutar IO_STDIN_READ"},
     { NULL, NULL, NULL }
 };
+
+int main(int argc, char* argv[]) {   
+    int i;
+    char* config_path = "../cpu/cpu.config";
+    
+    logger_cpu = iniciar_logger("../cpu/cpu.log", "cpu-log", LOG_LEVEL_INFO);
+    log_info(logger_cpu, "logger para CPU creado exitosamente.");
+
+    config = iniciar_config(config_path);
+
+    pthread_t hilo_id[4];
+
+    // Get info from cpu.config
+    
+    char* ip_memoria = config_get_string_value(config,"IP_MEMORIA");
+    char* puerto_memoria = config_get_string_value(config,"PUERTO_MEMORIA");
+    char* puerto_dispatch = config_get_string_value(config,"PUERTO_ESCUCHA_DISPATCH");
+    char* puerto_interrupt = config_get_string_value(config,"PUERTO_ESCUCHA_INTERRUPT");
+    
+    char* cant_ent_tlb = config_get_string_value(config,"CANTIDAD_ENTRADAS_TLB");
+    char* algoritmo_tlb = config_get_string_value(config,"ALGORITMO_TLB");
+    
+    log_info(logger_cpu, "%s\n\t\t\t\t\t%s\t%s\t", "INFO DE MEMORIA", ip_memoria, puerto_memoria);
+
+    // Abrir servidores
+    
+    server_dispatch = iniciar_servidor(logger_cpu, puerto_dispatch);
+    log_info(logger_cpu, "Servidor dispatch abierto");
+    server_interrupt = iniciar_servidor(logger_cpu, puerto_interrupt);
+    log_info(logger_cpu, "Servidor interrupt abierto");
+  
+    conexion_memoria = crear_conexion(ip_memoria, puerto_memoria);
+    enviar_operacion("CPU IS IN DA HOUSE", conexion_memoria, MENSAJE);
+
+    int cliente_fd_dispatch = esperar_cliente(server_dispatch, logger_cpu);
+    int cliente_fd_interrupt = esperar_cliente(server_interrupt, logger_cpu);
+
+    sem_init(&sem_ejecucion, 1, 0);
+
+    ArgsGestionarServidor args_dispatch = {logger_cpu, cliente_fd_dispatch};
+    ArgsGestionarServidor args_interrupt = {logger_cpu, cliente_fd_interrupt};
+    ArgsGestionarServidor args_memoria = {logger_cpu, conexion_memoria};
+
+    pthread_create(&hilo_id[0], NULL, gestionar_llegada_cpu, &args_dispatch);
+    pthread_create(&hilo_id[1], NULL, gestionar_llegada_cpu, &args_interrupt);
+    pthread_create(&hilo_id[2], NULL, gestionar_llegada_cpu, &args_memoria);
+
+    for(i = 0; i<5; i++){
+        pthread_join(hilo_id[i], NULL);
+    }
+    
+    liberar_conexion(conexion_memoria);
+    terminar_programa(logger_cpu, config);
+    
+    return 0;
+}
 
 void Execute(RESPONSE* response, regCPU* registers) {
     if (response != NULL) {
@@ -82,61 +140,7 @@ void procesar_contexto(regCPU* registros){
     }
 }
 
-int main(int argc, char* argv[]) {   
-    int i;
-    char* config_path = "../cpu/cpu.config";
-    
-    logger_cpu = iniciar_logger("../cpu/cpu.log", "cpu-log", LOG_LEVEL_INFO);
-    log_info(logger_cpu, "logger para CPU creado exitosamente.");
 
-    config = iniciar_config(config_path);
-
-    pthread_t hilo_id[4];
-
-    // Get info from cpu.config
-    
-    char* ip_memoria = config_get_string_value(config,"IP_MEMORIA");
-    char* puerto_memoria = config_get_string_value(config,"PUERTO_MEMORIA");
-    char* puerto_dispatch = config_get_string_value(config,"PUERTO_ESCUCHA_DISPATCH");
-    char* puerto_interrupt = config_get_string_value(config,"PUERTO_ESCUCHA_INTERRUPT");
-    
-    char* cant_ent_tlb = config_get_string_value(config,"CANTIDAD_ENTRADAS_TLB");
-    char* algoritmo_tlb = config_get_string_value(config,"ALGORITMO_TLB");
-    
-    log_info(logger_cpu, "%s\n\t\t\t\t\t%s\t%s\t", "INFO DE MEMORIA", ip_memoria, puerto_memoria);
-
-    // Abrir servidores
-    
-    int server_dispatch = iniciar_servidor(logger_cpu, puerto_dispatch);
-    log_info(logger_cpu, "Servidor dispatch abierto");
-    int server_interrupt = iniciar_servidor(logger_cpu, puerto_interrupt);
-    log_info(logger_cpu, "Servidor interrupt abierto");
-  
-    conexion_memoria = crear_conexion(ip_memoria, puerto_memoria);
-    enviar_operacion("CPU IS IN DA HOUSE", conexion_memoria, MENSAJE);
-
-    int cliente_fd_dispatch = esperar_cliente(server_dispatch, logger_cpu);
-    int cliente_fd_interrupt = esperar_cliente(server_interrupt, logger_cpu);
-
-    sem_init(&sem_ejecucion, 1, 0);
-
-    ArgsGestionarServidor args_dispatch = {logger_cpu, cliente_fd_dispatch};
-    ArgsGestionarServidor args_interrupt = {logger_cpu, cliente_fd_interrupt};
-    ArgsGestionarServidor args_memoria = {logger_cpu, conexion_memoria};
-
-    pthread_create(&hilo_id[0], NULL, gestionar_llegada_cpu, &args_dispatch);
-    pthread_create(&hilo_id[1], NULL, gestionar_llegada_cpu, &args_interrupt);
-    pthread_create(&hilo_id[2], NULL, gestionar_llegada_cpu, &args_memoria);
-
-    for(i = 0; i<5; i++){
-        pthread_join(hilo_id[i], NULL);
-    }
-    
-    liberar_conexion(conexion_memoria);
-    terminar_programa(logger_cpu, config);
-    
-    return 0;
-}
 
 void* gestionar_llegada_cpu(void* args){
 	ArgsGestionarServidor* args_entrada = (ArgsGestionarServidor*)args;
